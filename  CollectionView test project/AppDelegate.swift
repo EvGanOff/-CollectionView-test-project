2097238d592cdaa9e5e8839ecec90f8ee9331224
Evgeny Ganusenko <evgan66@gmail.comevgan66@gmail.comevgan66@gmail.com>
2021-12-20 13:08:56 +0300

    add NavigationBar

---  CollectionView test project/AppDelegate.swift
+++  CollectionView test project/AppDelegate.swift
@@ -10,27 +10,63 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        if #available(iOS 13.0, *) {
+            let tabBarAppearance: UITabBarAppearance = UITabBarAppearance()
+            tabBarAppearance.configureWithDefaultBackground()
+            tabBarAppearance.backgroundColor = UIColor.systemGray2
+            UITabBar.appearance().standardAppearance = tabBarAppearance
+            if #available(iOS 13, *) {
+                let appearance = UINavigationBarAppearance()
+                let appearanceTab = UITabBarItemAppearance()
+                appearance.configureWithOpaqueBackground()
+                UINavigationBar.appearance().standardAppearance = appearance
+                UINavigationBar.appearance().scrollEdgeAppearance = appearance
+            }
+
+            if #available(iOS 13.0, *) {
+                if #available(iOS 15.0, *) {
+                    UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+                } else {
+                    // Fallback on earlier versions
+                }
+            }
+        }
+
+        let tabBarController = UITabBarController()
+
+
+        let firstViewController = FirstNavigationController()
+        let firstNavigationController = UINavigationController(rootViewController: firstViewController)
+        firstViewController.tabBarItem = UITabBarItem(title: "Медиатека", image: .init(systemName: "photo.fill.on.rectangle.fill"), tag: 0)
+
+        let secondViewController = UIViewController()
+        secondViewController.view.backgroundColor = .systemBlue
+        secondViewController.tabBarItem = UITabBarItem(title: "Для Вас", image: .init(systemName: "heart.text.square.fill"), tag: 1)
+
+        let thirdViewController = UIViewController()
+        let thirdNavigationController = UINavigationController(rootViewController: thirdViewController)
+
+        thirdViewController.view.backgroundColor = .systemBrown
+        thirdViewController.tabBarItem = UITabBarItem(title: "Альбомы", image: .init(systemName: "square.stack.fill"), tag: 2)
+
+        let fourViewController = UIViewController()
+        fourViewController.view.backgroundColor = .systemGreen
+        fourViewController.tabBarItem = UITabBarItem(title: "Поиск", image: .init(systemName: "magnifyingglass"), tag: 3)
+
+        tabBarController.setViewControllers([
+            firstNavigationController,
+            secondViewController,
+            thirdNavigationController,
+            fourViewController], animated: true)
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+
+
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
